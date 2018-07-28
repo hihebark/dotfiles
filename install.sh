ea46fdfb56e7469e4356d8b1b1d2ffef6e448a51
hihebark <https://hihebark@github.com>
2018-07-28 16:36:20 +0100

    downloading and installing golang

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# From evilsocket dofiles
+#From evilsocket dofiles
 
 bold=$(tput bold)
 norm=$(tput sgr0)
@@ -19,13 +19,13 @@ PACKAGES=(
   libpcap-dev
 )
 
-echo " > sudo apt-get install ${bold}${PACKAGES[*]}${norm} -y"
+echo "[#] sudo apt install ${bold}${PACKAGES[*]}${norm} -y"
 read 
 chsh -s /usr/bin/zsh
 
 for file in data/*
 do
-    echo "Linking $file to ${bold}~/.$(basename $file)${norm} ..."
+    echo "[#] Linking $file to ${bold}~/.$(basename $file)${norm} ..."
     rm -rf ~/.$(basename $file)
     ln -s $(pwd)/$file ~/.$(basename $file)
 done
@@ -33,7 +33,7 @@ done
 if [ ! -d ./data/vim/bundle/vundle ]; then
     git clone http://github.com/gmarik/vundle.git ./data/vim/bundle/vundle
 fi
-
+echo "[#] Setting ${bold}Vim${norm} pluging ..."
 git clone https://github.com/Shougo/neocomplete.vim ~/.vim/bundle/neocomplete.vim
 git clone https://github.com/scrooloose/nerdtree ~/.vim/bundle/nerdtree
 git clone https://github.com/Xuyuanp/nerdtree-git-plugin ~/.vim/bundle/nerdtree-git-plugin
@@ -44,3 +44,7 @@ git clone https://github.com/fatih/vim-go ~/.vim/bundle/vim-go
 git clone https://github.com/terryma/vim-multiple-cursors ~/.vim/bundle/vim-multiple-cursors
 
 vim +BundleInstall +GoInstallBinaries +qa
+echo "[#] Installing go ..."
+cd ~/Downloads
+wget -c $(curl -s https://golang.org/dl/ | grep "download downloadBox" | grep "linux" | awk -F '"' '{print $4}')
+sudo tar -C /usr/local -xzf go1.*.*.linux-amd64.tar.gz
